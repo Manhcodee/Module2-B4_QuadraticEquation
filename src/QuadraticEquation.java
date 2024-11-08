964d0d9f21d2f0ffb81980ccc49480a84d644055
Manhcodee <phammanh2952@gmail.com>
2024-11-08 09:35:35 +0700

    fix

--- src/QuadraticEquation.java
+++ src/QuadraticEquation.java
@@ -10,11 +10,12 @@ public class QuadraticEquation {
         double c = new Scanner(System.in).nextDouble();
 
         Equation equation = new Equation();
-        double delta = equation.getDiscriminant();
+//        double delta = equation.getDiscriminant();
+        double delta = (b * b) - (4 * a * c);
         if (delta > 0) {
-            double root1 = (-b + Math.sqrt(delta) / 2 * a);
-            double root2 = (-b + Math.sqrt(delta) / 2 * a);
-            System.out.println("2 distinct solutions: x1 = :" + root1 + "x2 = " + root2);
+            double root1 = ((-b - Math.sqrt(delta)) / (2 * a));
+            double root2 = ((-b + Math.sqrt(delta)) / (2 * a));
+            System.out.println("2 distinct solutions: x1 = :" + root1 + " x2 = " + root2);
         } else if (delta == 0) {
             double root = -b / (2 * a);
             System.out.println("Solution: " + root);
